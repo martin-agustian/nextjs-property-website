b1f0835684a9cdf389b5d4aa85cba73844b47d60
martin <martinagustian@yahoo.com>
2025-04-07 15:48:40 +0700

    update footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -7,69 +7,75 @@ import EmailIcon from "../icons/email-icon";
 
 export default function Footer() {
   return (
-    <footer className="bg-mirage text-white py-10">
+    <footer className="section bg-mirage text-white !pb-[24px]">
       <div className="container">
-        <div className="text-[37px] text-center md:text-left font-[600]">
+        <div className="text-[48px] md:text-[37.2px] text-center md:text-left font-[600] leading-[44.64px] tracking-[-3%]">
           Dwell
         </div>
 
-        <div className="flex gap-16 md:gap-10 lg:gap-5 flex-col md:flex-row text-center md:text-left">
+        <div className="flex gap-[56px] flex-col lg:flex-row text-center md:text-left mt-[56px]">
           <div className="md:flex-3 order-1">
-            <div className="text-[24px] lg:text-[28px] font-manrope font-[600] leading-[120%] mt-5">
-              Stay informed about the <span className="text-mariner">latest property opportunities</span>
+            <div className="text-[24px] xl:text-[28px] text-left font-manrope font-[600] leading-[120%]">
+              Stay informed about the <br className="block lg:hidden"/> <span className="text-mariner">latest <br className="hidden lg:block" /> property opportunities</span>
             </div>
 
-            <div className="relative md:inline-block mt-5">
-              <input placeholder="benoitmacke@car" className="h-[40px] lg:h-[48px] w-[100%] md:w-[374px] bg-white text-black placeholder:text-cadet-grey rounded-full pl-3 pr-11" />
+            <div className="relative md:inline-block mt-[20px]">
+              <input placeholder="benoitmacke@car" className="h-[40px] xl:h-[48px] w-full md:w-[374px] bg-[#F8FAFC] text-[14px] text-black placeholder:text-cadet-grey font-[400] leading-[150%] rounded-full pl-[16px] pr-[42px]" />
 
-              <div className="size-[32px] flex items-center justify-center absolute top-[50%] translate-[-50%] right-[-10px] bg-tropical-blue rounded-full">
+              <div className="flex items-center justify-center size-[32px] absolute top-[50%] translate-y-[-50%] right-[4px] bg-tropical-blue rounded-full">
                 <ArrowIcon2 color="#0F172A" className="size-[12px]" />
               </div>
             </div>
 
-            <label htmlFor="agreement" className="flex items-center space-x-2 mt-4">
+            <label htmlFor="agreement" className="flex items-center space-x-[12px] mt-[16px]">
               <input type="checkbox" id="agreement" className="hidden peer" />
               <span className="size-[16px] flex items-center justify-center cursor-pointer transition-colors peer-checked:bg-mariner peer-checked:border-mariner peer-checked:before:content-['✔'] border-1 border-white rounded-sm peer-checked:before:text-[11px] p-1" />
-              <span className="text-[14px] font-[400] leading-[150%] cursor-pointer">I accept the privacy policy*.</span>
+              <span className="text-[14px] text-[#E2E8F0] font-[400] leading-[150%] cursor-pointer">I accept the privacy policy*.</span>
             </label>
           </div>
 
-          <ul className="md:flex-2 flex flex-col items-center md:items-start gap-3 text-[14px] font-[500] leading-[150%] order-3 md:order-2">
-            <li className="flex items-center gap-3 capitalize cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('geo:0,0?q=118+Maréchal+Street,+1000+Brussels')}>
-              <MapPinIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
-              118 Maréchal Street, 1000 Brussels
+          <ul className="md:flex-2 flex flex-col items-center md:items-start gap-[10px] text-[14px] font-[500] leading-[150%] order-3 md:order-2">
+            <li className="flex items-center lg:items-start gap-[8px] capitalize cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('geo:0,0?q=118+Maréchal+Street,+1000+Brussels')}>
+              <div className="flex items-center justify-center size-[24px]">
+                <MapPinIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
+              </div>
+              118 Maréchal Street, 1000 <br className="hidden lg:block" /> Brussels
             </li>
-            <li className="flex gap-3 cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('tel:+32460255201')}>
-              <PhoneIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
+            <li className="flex gap-[8px] cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('tel:+32460255201')}>
+              <div className="flex items-center justify-center size-[24px]">              
+                <PhoneIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
+              </div>
               +3 24 60 25 52 01
             </li>
-            <li className="flex gap-3 cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('mailto:contact@dwell.be')}>
-              <EmailIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
+            <li className="flex gap-[8px] cursor-pointer hover:text-dark-pastel-blue" onClick={() => window.open('mailto:contact@dwell.be')}>
+              <div className="flex items-center justify-center size-[24px]">                
+                <EmailIcon color="#72A5E8" className="size-[16px] min-w-[16px] min-h-[16px]" />
+              </div>
               contact@dwell.be
             </li>
           </ul>
 
-          <ul className="md:flex-1 flex flex-col gap-3 text-[14px] font-[500] leading-[150%] capitalize order-2 md:order-3">
-            <li className="cursor-pointer hover:text-dark-pastel-blue">service</li>
+          <ul className="md:flex-1 flex flex-col gap-[10px] text-[16px] md:text-[14px] font-[400] leading-[150%] capitalize order-2 md:order-3">
+            <li className="cursor-pointer hover:text-dark-pastel-blue">services</li>
             <li className="cursor-pointer hover:text-dark-pastel-blue">properties</li>
             <li className="cursor-pointer hover:text-dark-pastel-blue">contact</li>
             <li className="cursor-pointer hover:text-dark-pastel-blue">blog</li>
           </ul>
         </div>
 
-        <div className="flex gap-8 flex-col md:flex-row md:justify-between items-center text-[12px] mt-16">
-          <div className="flex gap-5 order-2 md:order-1">
-            <div className="flex gap-2">
+        <div className="flex gap-[20px] flex-col md:flex-row md:justify-between items-center text-[12px] text-[#F1F5F9] font-[400] leading-[16px] mt-[40px] md:mt-[100px]">
+          <div className="flex gap-[14px] order-2 md:order-1">
+            <div className="flex gap-[8px]">
               <div className="cursor-pointer" onClick={() => window.open('https://instagram.com')}>
-                <InstagramIcon color="white" className="size-[16px]" />
+                <InstagramIcon color="white" className="size-[14px]" />
               </div>
               <div className="cursor-pointer" onClick={() => window.open('https://linkedin.com')}>
-                <LinkedInIcon color="white" className="size-[16px] cursor-pointer" />
+                <LinkedInIcon color="white" className="size-[14px] cursor-pointer" />
               </div>
             </div>
             Eazyclick ©2024, all rights reserved
           </div>
-          <div className="flex gap-3 order-1 md:order-2">
+          <div className="flex gap-[12px] order-1 md:order-2">
             <span className="cursor-pointer hover:text-dark-pastel-blue">Terms & Conditions</span>
             <span className="cursor-pointer hover:text-dark-pastel-blue">Privacy Policy</span>
             <span className="cursor-pointer hover:text-dark-pastel-blue">Cookies</span>
